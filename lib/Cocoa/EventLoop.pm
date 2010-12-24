a3ce583e3f92d10ebdfdf43b4aacadecea94f292
Daisuke Murase <typester@cpan.org>
2010-12-24 15:05:31 +0900

    implemented io watcher and timer

--- lib/Cocoa/EventLoop.pm
+++ lib/Cocoa/EventLoop.pm
@@ -1,8 +1,49 @@
 package Cocoa::EventLoop;
 use strict;
-use warnings;
+use XSLoader;
 
 our $VERSION = '0.01';
+
+XSLoader::load __PACKAGE__, $VERSION;
+
+sub timer {
+    my ($class, %arg) = @_;
+
+    my $cb    = $arg{cb};
+    my $ival  = $arg{interval} || 0;
+    my $after = $arg{after} || 0;
+
+    my $timer = bless {}, 'Cocoa::EventLoop::timer';
+    __add_timer($timer, $after, $ival, $cb);
+
+    $timer;
+}
+
+sub Cocoa::EventLoop::timer::DESTROY {
+    __remove_timer($_[0]);
+}
+
+sub io {
+    my ($class, %arg) = @_;
+
+    my $fd = fileno($arg{fh});
+    defined $fd or $fd = $arg{fh};
+
+    my $mode = $arg{poll} eq 'r' ? 0 : 1;
+    my $io = bless {}, 'Cocoa::EventLoop::io';
+
+    __add_io($io, $fd, $mode, $arg{cb});
+
+    $io;
+}
+
+sub Cocoa::EventLoop::io::DESTROY {
+    __remove_io($_[0]);
+}
+
+1;
+
+__END__
 
 =head1 NAME
 
@@ -10,8 +51,17 @@ Cocoa::EventLoop - Module abstract (<= 44 characters) goes here
 
 =head1 SYNOPSIS
 
-  use Cocoa::EventLoop;
-  blah blah blah
+use Cocoa::EventLoop;
+
+my $timer = Cocoa::EventLoop->timer(
+    after => 10,
+    cb    => sub {
+        
+    },
+);
+
+my $io = Cocoa::EventLoop->io
+
 
 =head1 DESCRIPTION
 
@@ -36,5 +86,3 @@ The full text of the license can be found in the
 LICENSE file included with this module.
 
 =cut
-
-1;
